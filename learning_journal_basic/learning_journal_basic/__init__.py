c5c0c976235d51da2400013b29507b44415088cb
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-22 11:54:47 -0700

    another demo commit

--- learning_journal_basic/learning_journal_basic/__init__.py
+++ learning_journal_basic/learning_journal_basic/__init__.py
@@ -7,7 +7,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.routes')
-
+    config.include('.views')
     #scan needs to be the last thing before we return config.wsgi_app()
     config.scan()
     return config.make_wsgi_app()
--- learning_journal_basic/learning_journal_basic/views.py
+++ learning_journal_basic/learning_journal_basic/views.py
@@ -4,10 +4,14 @@
 #     return {'project': 'learning_journal_basic'}
 
 from pyramid.response import Response
+import os
+
+HERE = os.path.dirname(__file__)
 
 
 def my_view(request):
-    return Response("This is my first view!")
+    imported_text = open(os.path.join(HERE, 'sample.html')).read()
+    return Response(imported_text)
 
 
 def includeme(config):
